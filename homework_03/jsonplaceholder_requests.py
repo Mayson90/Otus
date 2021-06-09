696c6d567d47da809d98c273ed98de37f160737c
Andrey Mayskiy <andrey.mayskiy@ingrammicro.com>
2021-06-09 17:29:37 +0300

    change session in fetch_json

--- homework_03/jsonplaceholder_requests.py
+++ homework_03/jsonplaceholder_requests.py
@@ -9,9 +9,10 @@ USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
 
-async def fetch_json(session: ClientSession, url: str) -> dict:
-    async with session.get(url) as response:
-        return await response.json()
+async def fetch_json(url: str) -> dict:
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
 
 
 async def get_data() -> tuple:
